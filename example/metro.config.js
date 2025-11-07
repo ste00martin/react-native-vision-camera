4c7450219d72fddef2068c1d87552a554c9d8d5d
Stefan Martin <ste00martin@gmail.com>
2025-11-06 20:10:50 -0800

    getting metro to work

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,7 +1,7 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
 const path = require('path')
 const escape = require('escape-string-regexp');
-const pak = require('../package.json')
+const pak = require('../package/package.json')
 
 const root = path.resolve(__dirname, '..')
 const modules = Object.keys({ ...pak.peerDependencies })
@@ -13,7 +13,14 @@ const modules = Object.keys({ ...pak.peerDependencies })
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
+  projectRoot: __dirname,
   watchFolders: [root],
+
+  watcher: {
+    healthCheck: {
+      enabled: true,
+    },
+  },
 
   // We need to make sure that only one version is loaded for peerDependencies
   // So we block them at the root, and alias them to the versions in example's node_modules
@@ -26,7 +33,10 @@ const config = {
     extraNodeModules: modules.reduce((acc, name) => {
       acc[name] = path.join(__dirname, 'node_modules', name)
       return acc
-    }, {}),
+    }, {
+      // Add additional dependencies that need to be resolved from example's node_modules
+      'react-native-gesture-handler': path.join(__dirname, 'node_modules', 'react-native-gesture-handler'),
+    }),
   },
 
   transformer: {
@@ -39,4 +49,13 @@ const config = {
   },
 }
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config)
+const mergedConfig = mergeConfig(getDefaultConfig(__dirname), config)
+
+// Ensure projectRoot and watchFolders are explicitly set
+mergedConfig.projectRoot = __dirname
+mergedConfig.watchFolders = [root]
+
+// Reset cache on startup to avoid stale cache issues
+mergedConfig.resetCache = true
+
+module.exports = mergedConfig
